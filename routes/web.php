a83881e9a075e23586f2e12944279f13266a2879
rajadinata512-ops <rajadinata512@gmail.com>
2026-05-17 23:59:44 +0700

    change home route

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TransaksiController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/dashboard');
 });
 
 Route::get('/dashboard', [TransaksiController::class, 'index'])
