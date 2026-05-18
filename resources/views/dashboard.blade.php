d0dea56fdd62aa0dfb9e9ffc4750ae4f916eeee1
rajadinata512-ops <rajadinata512@gmail.com>
2026-05-19 03:18:00 +0700

    tolonggg

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -545,15 +545,11 @@ footer{
                 Kelola pemasukan dan pengeluaran duet mu pakcik
             </p>
         </div>
-
-        <form action="{{ route('logout') }}" method="POST">
+        <form action="{{ route('logout') }}" method="POST" style="margin-top:8px; flex-shrink:0;">
             @csrf
-
-            <button type="submit" class="delete-btn">
-                Logout
-            </button>
+            <button type="submit" class="logout-btn">Logout</button>
         </form>
-
+    </div>
     <!-- CARD -->
 
     <div class="cards">
